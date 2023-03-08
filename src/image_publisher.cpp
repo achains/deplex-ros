3f52f59bfa8f1ccaa5e287aa9286a377cf6544c5
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-03-08 23:53:57 +0300

    feat: simple image publisher

--- src/image_publisher.cpp
+++ src/image_publisher.cpp
@@ -1,6 +1,7 @@
-#include <chrono>
+#include <filesystem>
 #include <functional>
 #include <memory>
+#include <queue>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
@@ -8,39 +9,45 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node
-{
+class ImagePublisher : public rclcpp::Node {
  public:
-  MinimalPublisher()
-      : Node("minimal_publisher"), count_(0)
-  {
+  ImagePublisher() : Node("image_publisher") {
+    this->declare_parameter("image_dir", ".");
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalPublisher::timer_callback, this));
-
+    timer_ = this->create_wall_timer(30ms, std::bind(&ImagePublisher::timer_callback, this));
+    image_path_ = this->get_parameter("image_dir").get_parameter_value().get<std::string>();
+    directory_it_ = std::filesystem::directory_iterator(image_path_);
+    image_it_ = directory_it_;
   }
 
  private:
-  void timer_callback()
-  {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  void timer_callback() {
+    auto wd = this->get_parameter("image_dir").get_parameter_value().get<std::string>();
+    if (image_path_ != wd) {
+      image_path_ = wd;
+      directory_it_ = std::filesystem::directory_iterator(image_path_);
+      image_it_ = directory_it_;
+    }
 
-    publisher_->publish(message);
+    if (image_it_ != std::filesystem::end(directory_it_)) {
+      auto message = std_msgs::msg::String();
+      message.data = image_it_->path().string();
+      RCLCPP_INFO(this->get_logger(), "Publishing file: %s", message.data.c_str());
+      ++image_it_;
+      publisher_->publish(message);
+    }
   }
-  rclcpp::TimerBase::SharedPtr timer_;
+
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::filesystem::directory_iterator directory_it_;
+  std::filesystem::directory_iterator image_it_;
+  std::filesystem::path image_path_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<ImagePublisher>());
   rclcpp::shutdown();
   return 0;
 }
