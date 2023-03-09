dc6c053bb99de0471de75d96d9527ce04fd77582
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2023-03-10 01:07:58 +0300

    feat: simple image listener with deplex extractor

--- src/image_listener.cpp
+++ src/image_listener.cpp
@@ -1,25 +1,41 @@
 #include <memory>
 
+#include <deplex/deplex.h>
+
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
-{
+class MinimalSubscriber : public rclcpp::Node {
  public:
-  MinimalSubscriber()
-      : Node("minimal_subscriber")
-  {
+  MinimalSubscriber() : Node("image_listener") {
+    this->declare_parameter("config_path", ".");
+    this->declare_parameter("intrinsics_path", ".");
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-        "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-
+        "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
   }
 
  private:
-  void topic_callback(const std_msgs::msg::String & msg) const
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+  void topic_callback(const std_msgs::msg::String& msg) const {
+    auto image_path = msg.data;
+    if (image_path.substr(image_path.find_last_of('.') + 1) != "png") {
+      RCLCPP_INFO(this->get_logger(), "Received %s, NOT A PNG file!", image_path.c_str());
+      return;
+    }
+    auto config_path = this->get_parameter("config_path").get_parameter_value().get<std::string>();
+    auto intrinsics_path = this->get_parameter("intrinsics_path").get_parameter_value().get<std::string>();
+
+    auto image = deplex::utils::Image(image_path);
+    auto intrinsics = deplex::utils::readIntrinsics(intrinsics_path);
+    auto algorithm = deplex::PlaneExtractor(image.getHeight(), image.getWidth(), deplex::config::Config(config_path));
+    auto extracted_planes = algorithm.process(image.toPointCloud(intrinsics));
+
+    std::sort(extracted_planes.begin(), extracted_planes.end());
+    int planes_count =
+        std::distance(extracted_planes.begin(), std::unique(extracted_planes.begin(), extracted_planes.end()));
+
+    RCLCPP_INFO(this->get_logger(), "Processing %s\nPlanes found: %d", msg.data.c_str(), planes_count);
   }
+
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
